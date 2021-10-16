5074e1c9dbf20f8e95a4d168ba89099496ddcb62
Oznur Kibar <okibar99@gmail.com>
2021-10-16 17:27:01 +0300

    initial commit

---
+++ src/main/java/com/student/grocerystore/HelloController.java
@@ -0,0 +1,14 @@
+package com.student.grocerystore;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.student.grocerystore {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.student.grocerystore to javafx.fxml;
+    exports com.student.grocerystore;
+}
