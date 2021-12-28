8eb977daa62dbbfaa565b7833c9bf001d4805542
Your Name <you@example.com>
2021-12-28 16:03:13 +0200

    withDBconnection

--- src/main/java/com/student/grocerystore/HelloApplication.java
+++ src/main/java/com/student/grocerystore/HelloApplication.java
@@ -7,7 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import java.sql.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class HelloApplication extends Application {
+    private final String url = "jdbc:postgresql://localhost/postgres";
+    private final String user = "postgres";
+    private final String password = "  ";
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
@@ -16,8 +23,20 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 
     public static void main(String[] args) {
         launch();
+        HelloApplication app = new HelloApplication();
+        app.connect();
     }
 }
